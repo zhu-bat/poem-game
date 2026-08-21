e6f2a0488629cb423437dde4916eef9ffcbd02cf
Pointer Rain <gmckinlay04@gmail.com>
2026-08-22 00:15:38 +1000

    Switch to database

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 
-from models import *
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///poem_game.db'
+db = SQLAlchemy(app)
 
+from models import Server, Player
 
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -15,17 +23,36 @@ def hello_world():  # put application's code here
 def words_test():
     return render_template('words_test.html')
 
+@app.route('/api')
+def get_server_data():
+    server = Server.query.first()
+    if server:
+        return server.to_json()
+    else:
+        return {}
+
+
+@app.route('/submit-poem', methods=['POST'])
+def submit_poem():
+    
+
 
 @app.route('/bussy')
 def bussy():
     server = Server()
-    bussy = Player('Bussy')
-    server.add_player(bussy)
-    zubat = Player('Zubat')
-    server.add_player(zubat)
+    db.session.add(server)
+    db.session.commit()
+    bussy = Player(name='Bussy', server=server.id)
+    db.session.add(bussy)
+    # server.add_player(bussy)
+    zubat = Player(name='Zubat', server=server.id)
+    db.session.add(zubat)
+    # server.add_player(zubat)
+    db.session.commit()
 
     return server.to_json()
 
 
 if __name__ == '__main__':
+
     app.run()
--- models.py
+++ models.py
@@ -1,73 +1,67 @@
 import random
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app import db
 
 words = ["a", "an", "the", "and", "all", "am", "is"]
 
-    
 
-class Server:
+class Server(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    round: Mapped[int] = mapped_column(default=0)
 
-    def __init__(self):
-        self.players = []
-
-    # def wait_for_players(self):
-    #     while (1):
-    #         if player add player    
-
-    def add_player(self, player):
-        self.players.append(player)
+    def get_players(self):
+        return Player.query.filter_by(server=self.id).all()
 
     def display_players(self):
         str = ""
-        for p in self.players:
+        for p in self.get_players():
             str += p.name
             str += '\n'
         return str
 
     def generate_words(self):
-
         xs = []
         for i in range(5):
             xs.append(words[random.randint(0, len(words)-1)])
         return xs
 
     def send_voting_poems(self, player):
-        return [p.poem for p in self.players if p != player]
+        return [p.poem for p in self.get_players() if p != player]
 
     def to_json(self):
-        return { "players": [p.to_json() for p in self.players] }
-    
+        return { "players": [p.to_json() for p in self.get_players()] }
 
 
-
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-        self.poem = None
-        self.vote = None
-
+class Player(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    server: Mapped[int] = mapped_column(db.ForeignKey(Server.id))
+    name: Mapped[str] = mapped_column(String(20))
+    score: Mapped[int] = mapped_column(default=0)
+    poem: Mapped[str] = mapped_column(String(500), nullable=True, default=None)
+    vote: Mapped[int] = mapped_column(nullable=True, default=None)
 
     def get_words(self, server):
         return server.generate_words()
-    
+
     def set_poem(self, str):
         self.poem = str
-    
+
     def get_voting_poems(self, server):
         return server.send_voting_poems(self)
- 
+
     def set_vote(self, player):
         self.vote = player
 
-    
     def update_score(self, n):
         self.score += n
 
     def to_json(self):
-        return { "name": str(self.name), 
+        return { "name": str(self.name),
                  "score": int(self.score),
                  "poem": str(self.poem),
                  "vote": str(self.vote)
-                 }    
-
+                 }

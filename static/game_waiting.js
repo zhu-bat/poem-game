e7a42f58d0a983d28b69e55eebee3627d37ed596
Pointer Rain <gmckinlay04@gmail.com>
2026-08-22 21:41:42 +1000

    List players during waits

--- static/game_waiting.js
+++ static/game_waiting.js
@@ -1,11 +1,12 @@
 const getJSON = async url => {
   const response = await fetch(url);
-  if(!response.ok) // check if response worked (no 404 errors etc...)
+  if(!response.ok)
     throw new Error(response.statusText);
 
-  const data = response.json(); // get JSON from the response
-  return data; // returns a promise, which resolves to this data value
+  return response.json();
 }
+
+const playerListContainer = document.getElementById('player-list');
 
 function waitForPhase(phase) {
   const intervalId = setInterval(() => {
@@ -25,9 +26,48 @@ function waitForPhase(phase) {
           clearInterval(intervalId);
           window.location.href = "/game";
         }
+        if (phase === 'writing') {
+            drawVipPlayerList(Object.values(data['players']));
+        } else {
+            let subType = phase === 'voting' ? 'poem_submitted' : 'vote_submitted';
+            drawPlayerList(Object.values(data['players']), subType);
+        }
       })
       .catch(error => {
         console.error(error);
       });
   }, 3000);
+}
+
+
+function drawPlayerList(players, sub_type) {
+    playerListContainer.innerHTML = '';
+    players.forEach(player => {
+        const playerDiv = document.createElement('div');
+        const statusSpan = document.createElement('span');
+        statusSpan.textContent = player[sub_type] ? '✔' : '✘';
+        statusSpan.style.display = 'inline-block';
+        statusSpan.style.width = '2em';
+        const nameSpan = document.createElement('span');
+        nameSpan.textContent = player['name'];
+        playerDiv.appendChild(statusSpan);
+        playerDiv.appendChild(nameSpan);
+        playerListContainer.appendChild(playerDiv);
+    });
+}
+
+function drawVipPlayerList(players) {
+    playerListContainer.innerHTML = '';
+    players.forEach(player => {
+        const playerDiv = document.createElement('div');
+        const vipSpan = document.createElement('span');
+        vipSpan.textContent = player['vip'] ? '*' : '';
+        vipSpan.style.display = 'inline-block';
+        vipSpan.style.width = '2em';
+        const nameSpan = document.createElement('span');
+        nameSpan.textContent = player['name'];
+        playerDiv.appendChild(vipSpan);
+        playerDiv.appendChild(nameSpan);
+        playerListContainer.appendChild(playerDiv);
+    });
 }

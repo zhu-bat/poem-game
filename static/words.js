ecfc8a02f3ea65d2a33019e91da5968a249e5cd9
zhu-bat <kiarazhu386@gmail.com>
2026-08-23 12:26:28 +1000

    css shi

--- static/words.js
+++ static/words.js
@@ -1,6 +1,14 @@
-const timeout = setTimeout(change, 2000);
+const timeout1 = setTimeout(change1, 1000);
+const timeout2 = setTimeout(change2, 2000);
 
-function change() {
-    document.querySelector("workspace").style.display = "block";
+function change1() {
+    document.getElementById("start").style.display = "block";
+}
+
+
+
+function change2() {
+    document.querySelector(".header").style.display = "none";
+    document.querySelector(".workspace").style.display = "block";
 }
 
